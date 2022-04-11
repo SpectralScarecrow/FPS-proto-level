2d4bbba72cf7cd7cc13e0616a3156829baff59ec
SpectralScarecrow <90359319+SpectralScarecrow@users.noreply.github.com>
2022-04-11 18:46:09 -0400

    updated to have win/lose conditions

--- CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
+++ CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
@@ -7,14 +7,20 @@ public class GameManager : MonoBehaviour
 {
     [Header("Game")]
     public Player player;
+    public GameObject enemySpawner;
+
     [Header("UI")]
     public Text ammoText;
     public Text healthText;
+    public Text enemyText;
+    public Text infotext;
+    public Text losetext;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        infotext.gameObject.SetActive(false);
+        losetext.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +28,25 @@ public class GameManager : MonoBehaviour
     {
         ammoText.text = "Ammo: " + player.Ammo;
         healthText.text = "Health: " + player.Health;
+        
+
+        int killedEnemies = 0;
+        foreach (Enemy enemy in enemySpawner.GetComponentsInChildren<Enemy>())
+        {
+            //We are going to iterate over every enemies
+            if(enemy.Killed == false)
+            {
+                killedEnemies++;
+            }
+        }
+        enemyText.text = "Enemies: " + killedEnemies;
+        if (killedEnemies == 0)
+        {
+            infotext.gameObject.SetActive(true);
+        }
+        if (player.Killed == true)
+        {
+            losetext.gameObject.SetActive(true);
+        }
     }
 }
